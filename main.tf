65d87f9bccd13098ee231b4c7aa2c969fcd7f0c5
Ubuntu <ubuntu@ip-172-31-4-0.us-east-2.compute.internal>
2023-01-14 22:44:37 +0000

    asg

--- main.tf
+++ main.tf
@@ -131,13 +131,22 @@ resource "aws_security_group" "dev_sg" {
     protocol    = "tcp"
     cidr_blocks = ["10.1.0.0/16"]
   }
+
+  ingress {
+    description = "80 from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.1.0.0/16"]    
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["10.1.0.0/16"]
   }
-}
+
 
 resource "aws_security_group" "alb" {
   name        = "alb"
