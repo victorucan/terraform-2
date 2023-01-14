e643d71cf5a78ad1ca8c336d58928f34bad27955
Ubuntu <ubuntu@ip-172-31-4-0.us-east-2.compute.internal>
2023-01-14 19:34:33 +0000

    asg-alb

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ resource "aws_route_table" "rt1" {
     gateway_id = aws_internet_gateway.gw.id
   }
   tags = {
-    Name = "rt_Pub"
+    Name = "rt_Public"
   }
 }
 
@@ -97,22 +97,22 @@ resource "aws_route_table" "rt2" {
 
 resource "aws_route_table_association" "as1" {
   subnet_id      = aws_subnet.subpriv1.id
-  route_table_id = aws_route_table.rt2.id
+  route_table_id = aws_route_table.rt1.id
 }
 
 resource "aws_route_table_association" "as2" {
   subnet_id      = aws_subnet.subpriv2.id
-  route_table_id = aws_route_table.rt2.id
+  route_table_id = aws_route_table.rt1.id
 }
 
 resource "aws_route_table_association" "as3" {
   subnet_id      = aws_subnet.subpub1.id
-  route_table_id = aws_route_table.rt1.id
+  route_table_id = aws_route_table.rt2.id
 }
 
 resource "aws_route_table_association" "as4" {
   subnet_id      = aws_subnet.subpub2.id
-  route_table_id = aws_route_table.rt1.id
+  route_table_id = aws_route_table.rt2.id
 }
 
 resource "aws_security_group" "dev_sg" {
@@ -134,43 +134,126 @@ resource "aws_security_group" "dev_sg" {
   }
 }
 
-resource "aws_key_pair" "key" {
-  key_name   = "mykey"
-  public_key = file("/home/ubuntu/.ssh/mykey.pub")
+data "aws_ami" "Nginx" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # jammy
 }
 
-resource "aws_instance" "web1" {
+resource "aws_launch_configuration" "as_conf" {
+  name_prefix   = "terraform-lc-nginx"
+  image_id      = data.aws_ami.Nginx.id
   instance_type = "t2.micro"
-  ami           = "ami-0ff39345bd62c82a5"
-  key_name = aws_key_pair.key.id
-  subnet_id = aws_subnet.subpriv1.id
-  user_data = file("userdata.tpl")
-  vpc_security_group_ids = [aws_security_group.dev_sg.id]
-  associate_public_ip_address = false
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_placement_group" "test" {
+  name     = "test"
+  strategy = "cluster"
+}
+
+resource "aws_autoscaling_group" "ASG" {
+  name                      = "terraform-asg"
+  max_size                  = 5
+  min_size                  = 1
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 2
+  force_delete              = true
+  placement_group           = aws_placement_group.test.id
+  launch_configuration      = aws_launch_configuration.as_conf.id
+  vpc_zone_identifier       = [aws_subnet.subpriv1.id, aws_subnet.subpriv2.id]
+
+  initial_lifecycle_hook {
+    name                 = "foobar"
+    default_result       = "CONTINUE"
+    heartbeat_timeout    = 2000
+    lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
+
+    notification_metadata = <<EOF
+{
+  "foo": "bar"
+}
+EOF
+
+    notification_target_arn = "arn:aws:sqs:us-east-1:444455556666:queue1*"
+    role_arn                = "arn:aws:iam::123456789012:role/S3Access"
+  }
+
+  tag {
+    key                 = "foo"
+    value               = "bar"
+    propagate_at_launch = true
+  }
+
+  timeouts {
+    delete = "15m"
+  }
+
+  tag {
+    key                 = "lorem"
+    value               = "ipsum"
+    propagate_at_launch = false
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name        = "alb"
+  description = "alb network traffic"
+  vpc_id      = aws_vpc.pri_vpc.id
+
+  ingress {
+    description = "80 from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.dev_sg.id]
+  }
 
   tags = {
-    Name = "nginx1"
-  }
-  
-  root_block_device {
-    volume_size= 10
+    Name = "allow traffic"
   }
 }
 
-resource "aws_instance" "web2" {
-  instance_type = "t2.micro"
-  ami           = "ami-0ff39345bd62c82a5"
-  key_name = aws_key_pair.key.id
-  subnet_id = aws_subnet.subpriv2.id
-  user_data = file("userdata.tpl")
-  vpc_security_group_ids = [aws_security_group.dev_sg.id]
-  associate_public_ip_address = false
+resource "aws_lb" "LB" {
+  name               = "Terraform-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = [aws_subnet.subpriv1.id, aws_subnet.subpriv2.id]
+
+  enable_deletion_protection = false
+}
+  /*
+
+  access_logs {
+    bucket  = aws_s3_bucket.lb_logs.bucket
+    prefix  = "test-lb"
+    enabled = true
+  }
 
   tags = {
-    Name = "nginx2"
-  }
-  
-  root_block_device {
-    volume_size= 10
+    Environment = "production"
   }
 }
+*/
